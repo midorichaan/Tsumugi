7126921f7a2dc40de7daa8867be63a4b15c37656
midorichan <furandorusukaret.jp@gmail.com>
2022-03-29 22:06:06 +0900

    Update mido_presence.py

--- cogs/mido_presence.py
+++ cogs/mido_presence.py
@@ -7,13 +7,18 @@ class mido_presence(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self._fired = {}
 
     #on_presence_update
     @commands.Cog.listener()
     async def on_presence_update(self, bef, af):
         if not af.activity:
+            if self._fired.get(af.id, None):
+                del self._fired[af.id]
             return
         if af.id in self.bot.instance["ids"]:
+            self._fired[af.id] = 1
+            self._fired[af.id] += 1
             channel = self.bot.get_channel(self.bot.instance["channels"]["presence"])
             e = discord.Embed(
                 description=f"__**{af.activity.name}**__ をプレイ中",
@@ -21,7 +26,8 @@ class mido_presence(commands.Cog):
                 timestamp=datetime.datetime.now()
             )
             e.set_author(name=f"{af} ({af.id})", icon_url=af.avatar.replace(static_format="png"))
-            await channel.send(embed=e)
+            if self._fired[af.id] == 3:
+                await channel.send(embed=e)
 
 async def setup(bot):
     if not bot.instance.get("channels", None):
