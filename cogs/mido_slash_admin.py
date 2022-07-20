71986017aa412fee3716006e9d6222d952ed2710
midorichan <furandorusukaret.jp@gmail.com>
2022-07-20 15:13:22 +0900

    eval add

--- cogs/mido_slash_admin.py
+++ cogs/mido_slash_admin.py
@@ -2,10 +2,72 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+import io
+import textwrap
+from contextlib import redirect_stdout
+from lib import utils
+
 class mido_slash_admin(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self._
+
+    #eval
+    @app_commands.command(name="eval", description="Pythonのコードを評価します")
+    @app_commands.describe(code="評価するコード")
+    async def _eval(self, interact: discord.Interaction, *, code: str=None):
+        if not code:
+            return await interact.response.send_message(
+                content=f"> 評価するコードを入力してください",
+                ephemeral=True
+            )
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'self': self,
+            '_': self._
+        }
+
+        env.update(globals())
+        code = utils.cleanup_code(code)
+        stdout = io.StringIO()
+        to_compile = f'async def func():\n{textwrap.indent(code, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as exc:
+            return await interact.response.send_message(
+                content=f"```py\n{exc.__class__.__name__}: {exc}\n```",
+                ephemeral=True
+            )
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as exc:
+            value = stdout.getvalue()
+            return await interact.response.send_message(
+                content=f'```py\n{value}{traceback.format_exc()}\n```',
+                ephemeral=True
+            )
+        else:
+            value = stdout.getvalue()
+
+            if ret is None:
+                if value:
+                    await interact.response.send_message(
+                        content=f'```py\n{value}\n```',
+                        ephemeral=True
+                    )
+            else:
+                self._ = ret
+                await interact.response.send_message(
+                    content=f'```py\n{value}{ret}\n```',
+                    ephemeral=True
+                )
 
     #group instance
     group = app_commands.Group(
