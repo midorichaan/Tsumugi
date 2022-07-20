6a7bdf41bdea7f5fcc1aec42913b6461fa3ed18a
midorichan <furandorusukaret.jp@gmail.com>
2022-07-20 13:40:29 +0900

    Create mido_slash_admin.py

---
+++ cogs/mido_slash_admin.py
@@ -0,0 +1,78 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class mido_slash_admin(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    #group instance
+    group = app_commands.Group(
+        name="admin",
+        description="管理者用コマンド"
+    )
+
+    #admin reload
+    @group.command(name="reload", description="ファイルの再読み込みを行います")
+    @app_commands.describe(module="再読み込みするCog")
+    async def _reload(self, interact: discord.Interaction, module: str=None):
+        if not module:
+            return await interact.response.send_message(
+                content="> 再読み込みするCogを指定してください"
+            )
+
+        try:
+            await self.bot.reload_extension(module)
+        except Exception as exc:
+            return await interact.response.send_message(
+                content="> エラー \n```py\n{exc}\n```"
+            )
+        else:
+            return await interact.response.send_message(
+                content=f"> {module} を再読み込みしました"
+            )
+
+    #admin load
+    @group.command(name="load", description="ファイルを読み込みます")
+    @app_commands.describe(module="読み込むCog")
+    async def _load(self, interact: discord.Interaction, module: str=None):
+        if not module:
+            return await interact.response.send_message(
+                content="> 読み込むCogを指定してください"
+            )
+
+        try:
+            await self.bot.load_extension(module)
+        except Exception as exc:
+            return await interact.response.send_message(
+                content="> エラー \n```py\n{exc}\n```"
+            )
+        else:
+            return await interact.response.send_message(
+                content=f"> {module} を読み込みました"
+            )
+
+    #admin unload
+    @group.command(name="load", description="ファイルをunloadします")
+    @app_commands.describe(module="unloadするCog")
+    async def _load(self, interact: discord.Interaction, module: str=None):
+        if not module:
+            return await interact.response.send_message(
+                content="> unloadするCogを指定してください"
+            )
+
+        try:
+            await self.bot.unload_extension(module)
+        except Exception as exc:
+            return await interact.response.send_message(
+                content="> エラー \n```py\n{exc}\n```"
+            )
+        else:
+            return await interact.response.send_message(
+                content=f"> {module} をunloadしました"
+            )
+
+#setup
+async def setup(bot):
+    await bot.add_cog(mido_slash_admin(bot))
