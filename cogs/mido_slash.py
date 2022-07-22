7ff881fdaa5913eb4a7f26b8723058c63a409629
midorichan <furandorusukaret.jp@gmail.com>
2022-07-22 11:57:54 +0900

    Update mido_slash.py

--- cogs/mido_slash.py
+++ cogs/mido_slash.py
@@ -3,6 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 
 import time
+from lib import views, utils
 
 class mido_slash(commands.Cog):
 
@@ -28,6 +29,72 @@ class mido_slash(commands.Cog):
         await interact.edit_original_message(
             content=f"> 🏓Pong! \nPing: {ping}ms \nWebSocket: {ws}ms"
         )
+
+    #punish
+    @app_commands.command(
+        name="punish",
+        description="指定ユーザーを処罰します"
+    )
+    @app_commands.describe(target="処罰を行うユーザー")
+    async def _punish(self, interact: discord.Interaction, target: str=None, reason: str=None):
+        if not target:
+            return await interact.response.send_message(
+                content=f"> 対象ユーザーを指定してください"
+            )
+        if not reason:
+            reason = None
+
+        try:
+            interact.bot = interact.client or self.bot
+            target = await utils.FetchUserConverter().convert(interact, str(target))
+        except Exception as exc:
+            return await interact.response.send_message(
+                content=f"> メンバー {target} は見つかりませんでした"
+            )
+
+        drop = views.PunishmentDropdown()
+        view = views.BasicView().add_item(drop)
+
+        try:
+            await interact.response.send_message(
+                content="> 処罰の種類を選択してください",
+                view=view
+            )
+
+            b = await view.wait()
+            if b:
+                return await interact.response.send_message(
+                    content="> タイムアウトしました、最初からやり直してください"
+                )
+
+            if drop._value == "punish-kick":
+                try:
+                    await interact.guild.kick(target, reason=reason)
+                except:
+                    return await interact.response.send_message(
+                        content=f"> メンバーをKickできませんでした"
+                    )
+                else:
+                    return await interact.response.send_message(
+                        content=f"> {target} ({target.id})をサーバーからKickしました"
+                    )
+            elif drop._value == "punish-ban":
+                try:
+                    await interact.guild.ban(target, reason=reason)
+                except:
+                    return await interact.response.send_message(
+                        content=f"> メンバーをBanできませんでした"
+                    )
+                else:
+                    return await interact.response.send_message(
+                        content=f"> {target} ({target.id})をサーバーからBanしました"
+                    )
+            elif drop._value == "punish-timeout":
+                pass
+        except Exception as exc:
+            return await interact.response.send_message(
+                content=f"> エラー \n```py\n{exc}\n```"
+            )
 
 async def setup(bot):
     await bot.add_cog(mido_slash(bot))
